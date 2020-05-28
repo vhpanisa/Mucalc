0f0342bacdc090b677548b1b7555f22e19c0142d
Victor Panisa <wfxpanisa@users.noreply.github.com>
2020-05-27 21:17:14 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -47,7 +47,7 @@ self.addEventListener('fetch', (evt) => {
           .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
-                  return cache.match('offline.html');
+                  return cache.match('index.html');
                 });
           })
   );
